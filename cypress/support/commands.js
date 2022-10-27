b3d0d43c1c869e9c7deadbf2adb7456269c79c27
unknown <you@example.com>
2022-10-27 14:40:09 -0300

    Actualizacion

--- cypress/support/commands.js
+++ cypress/support/commands.js
@@ -23,3 +23,52 @@
 //
 // -- This will overwrite an existing command --
 // Cypress.Commands.overwrite('visit', (originalFn, url, options) => { ... })
+const Ajv = require("ajv");
+const ajv = new Ajv();
+require("cypress-grep")();
+import { addMatchImageSnapshotCommand } from "cypress-image-snapshot/command";
+
+addMatchImageSnapshotCommand();
+
+/**
+ *  Llama al servicio acorde al primer parametro enviado
+ * @method callService
+ * @param {string} lastUrl -  Última parte del endpoint que se concatenará con el resto
+ * @param {string} fileName -   Nombre del archivo de salida
+ */
+Cypress.Commands.add("callService", (lastUrl, fileName, schema = false) => {
+  cy.request({
+    method: "GET",
+    url:
+      "https://edenapi.edenentradas.com.ar/edenventarestapi2/api/contenido/" +
+      lastUrl,
+  }).then((response) => {
+    expect(response.status).to.eq(200);
+    cy.log(JSON.stringify(response));
+    cy.writeFile("cypress/fixtures/eden/" + fileName + ".json", response.body);
+
+    if (!schema) {
+      cy.log("No hay validación de esquema");
+    } else {
+      cy.fixture("schemas/" + schema).then((JsonFile) => {
+    
+        //nueva linea 
+
+        const validate = ajv.compile(JsonFile);
+        const valid = validate(response.body);
+        if (!valid) {
+          cy.log("Hay un error en el esquema");
+        } else {
+          cy.log("El esquema está bien");
+        }
+      }); //Linea nueva
+    }
+  });
+});
+
+addMatchImageSnapshotCommand({
+  failureThreshold: 0.03, // threshold for entire image
+  failureThresholdType: "percent", // percent of image or number of pixels
+  customDiffConfig: { threshold: 0.5 }, // threshold for each pixel
+  capture: "viewport", // capture viewport in screenshott
+});
